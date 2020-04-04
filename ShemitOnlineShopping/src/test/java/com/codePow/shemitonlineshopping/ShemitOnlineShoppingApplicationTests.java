9bdb0a8cb5e0ef18a8444742a6e77b93d92629d2
Melakeselam Moges <mmengistu@miu.edu>
2020-04-03 22:46:32 -0500

    First Commit

---
+++ ShemitOnlineShopping/src/test/java/com/codePow/shemitonlineshopping/ShemitOnlineShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codePow.shemitonlineshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShemitOnlineShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
